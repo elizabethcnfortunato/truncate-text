b39ac55e17f91b5110dc81025f71814667f96c45
leandronsp <leandronsp@gmail.com>
2011-04-13 19:59:34 -0300

    correction at the original element into script

--- truncate.js
+++ truncate.js
@@ -1,23 +1,28 @@
 jQuery.fn.truncate = function(maxCharacters) {
   return this.each(function(){
 	  
+	  var origin = $(this);
+	  
 	  var len = $(this).text().length;
 	  if (len > maxCharacters) {
-		  var first = $(this).text().substring(0, maxCharacters);
-		  var truncated = $(this).text().substring(maxCharacters, len);
-		  $(this).html(first);
-		  $(this).append("<span id=\"truncate\" style=\"display:none\"><\/span>");
+		  var truncated = $(this).text().substring(0, maxCharacters);
+		  truncated = truncated.replace(/\w+$/, '');
+		  $(this).after("<span id=\"truncate\" style=\"display:none\"><\/span>");
+		  $(this).hide();
 		  $("#truncate").html(truncated);	
-		  $(this).after("<br><a id=\"toggle\" href=\"#\">more<\/a>");
+		  $("#truncate").after("<br><a id=\"toggle\" href=\"#\">more<\/a>");
+		  $("#truncate").show();		  
 	  }
 	  $("#toggle").click(function(event) {
 		  event.preventDefault();
-		  if ($(this).html() == "more") {
+		  if ($(this).html() == "hide") {
+			  $(origin).hide();
 			  $("#truncate").show();
-			  $(this).html("hide");
+			  $(this).html("more");
 		  } else {
 			  $("#truncate").hide();
-			  $(this).html("more");
+			  $(origin).show();
+			  $(this).html("hide");
 		  }
 	  });
 
